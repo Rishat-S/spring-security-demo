7ba521e3d9717409586a41cdfb3aa06a511b2180
Rishat Suleymanov <spk_irbis@mail.ru>
2021-09-01 19:42:46 +0300

    Initial commit

---
+++ src/main/java/ru/netology/springsecuritydemo/controller/MainController.java
@@ -0,0 +1,19 @@
+package ru.netology.springsecuritydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from hello";
+    }
+
+    @GetMapping("/hello1")
+    public String hello1() {
+        return "Hello from hello1";
+    }
+
+}
