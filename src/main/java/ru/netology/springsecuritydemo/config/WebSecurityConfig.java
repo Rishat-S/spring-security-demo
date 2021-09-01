ac2c3a5bd25bb466e639152d0abc41b1352029c2
Rishat Suleymanov <spk_irbis@mail.ru>
2021-09-01 21:08:01 +0300

    Added permisions

--- src/main/java/ru/netology/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/ru/netology/springsecuritydemo/config/WebSecurityConfig.java
@@ -4,20 +4,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Rishat").password("{noop}1234").roles("READ")
-                .and().withUser("Rinat").password("{noop}1234").roles("WRITE");
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication().withUser("Rishat").password(encoder.encode("1234")).roles("READ")
+                .and().withUser("Rinat").password(encoder.encode("1234")).roles("WRITE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().and()
-                .authorizeRequests().antMatchers("/hello").permitAll()
+                .authorizeRequests().antMatchers("/hello").hasRole("READ")
+                .and()
+                .authorizeRequests().antMatchers("/hello1").hasRole("WRITE")
                 .anyRequest().authenticated();
     }
 }
