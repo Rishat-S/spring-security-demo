2fd934429e8d0a5331de1acd6871386994f664bf
Rishat Suleymanov <spk_irbis@mail.ru>
2021-09-01 20:36:49 +0300

    Added method comfigure

--- src/main/java/ru/netology/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/ru/netology/springsecuritydemo/config/WebSecurityConfig.java
@@ -1,11 +1,19 @@
 package ru.netology.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("Rishat").password("1234")
+                .and().withUser("Rinat").password("1234");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().and()
