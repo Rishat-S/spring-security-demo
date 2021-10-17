f3e6aa516874f13f9f39eafde47c4e7fb869f7e6
Rishat <spk_irbis@mail.ru>
2021-10-17 14:46:18 +0300

    AuthController added

--- src/main/java/ru/netology/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/ru/netology/springsecuritydemo/config/SecurityConfig.java
@@ -25,7 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/auth/login").permitAll()
+                .defaultSuccessUrl("/auth/success");
     }
 
     @Bean
