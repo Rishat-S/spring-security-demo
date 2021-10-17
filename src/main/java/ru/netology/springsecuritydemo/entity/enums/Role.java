3fff7f6d65b37d4fd195bd1918ac1d876d1920e1
Rishat <spk_irbis@mail.ru>
2021-10-17 13:10:06 +0300

    Role edited

--- src/main/java/ru/netology/springsecuritydemo/entity/enums/Role.java
+++ src/main/java/ru/netology/springsecuritydemo/entity/enums/Role.java
@@ -1,6 +1,9 @@
 package ru.netology.springsecuritydemo.entity.enums;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum Role {
     USER(Set.of(Permission.USERS_READ)),
@@ -14,5 +17,11 @@ public enum Role {
 
     public Set<Permission> getPermissions() {
         return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
     }
 }
