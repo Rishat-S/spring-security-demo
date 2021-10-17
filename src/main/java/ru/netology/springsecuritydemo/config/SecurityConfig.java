1269ecd527ceefd2e568375169565c0c1e6f77e8
Rishat <spk_irbis@mail.ru>
2021-10-17 13:56:15 +0300

    prePostEnable true Preauthorize

--- src/main/java/ru/netology/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/ru/netology/springsecuritydemo/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package ru.netology.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,20 +11,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import ru.netology.springsecuritydemo.entity.enums.Permission;
 import ru.netology.springsecuritydemo.entity.enums.Role;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/**").hasAuthority(Permission.USERS_READ.getPermission())
-                .antMatchers(HttpMethod.POST,"/api/**").hasAuthority(Permission.USERS_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE,"/api/**").hasAuthority(Permission.USERS_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/ru/netology/springsecuritydemo/controller/UserRestController.java
+++ src/main/java/ru/netology/springsecuritydemo/controller/UserRestController.java
@@ -1,5 +1,6 @@
 package ru.netology.springsecuritydemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.springsecuritydemo.entity.User;
 
@@ -22,6 +23,7 @@ public class UserRestController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('users:read')")
     public User getById(@PathVariable Long id) {
         return users.stream()
                 .filter(user -> user.getId().equals(id))
@@ -29,12 +31,14 @@ public class UserRestController {
     }
 
     @PostMapping()
+    @PreAuthorize("hasAuthority('users:write')")
     public User create(@RequestBody User user) {
         this.users.add(user);
         return user;
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('users:write')")
     public void deleteById(@PathVariable Long id) {
         this.users.removeIf(user -> user.getId().equals(id));
     }
