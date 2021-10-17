cc1cafd8ed0cdf7bb376412d2f268c89d0605a9b
Rishat <spk_irbis@mail.ru>
2021-10-17 13:06:03 +0300

    Permission added

--- src/main/java/ru/netology/springsecuritydemo/entity/enums/Role.java
+++ src/main/java/ru/netology/springsecuritydemo/entity/enums/Role.java
@@ -1,6 +1,18 @@
 package ru.netology.springsecuritydemo.entity.enums;
 
+import java.util.Set;
+
 public enum Role {
-    USER,
-    ADMIN
+    USER(Set.of(Permission.USERS_READ)),
+    ADMIN(Set.of(Permission.USERS_READ, Permission.USERS_WRITE));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
 }
